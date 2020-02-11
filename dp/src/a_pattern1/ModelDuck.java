95e85280ccee850c528b074f300ed31bfdf94afa
Keunhwa Lee <drg9830@gmail.com>
2020-02-11 19:56:46 +0900

    Bad CTO

--- dp/src/a_pattern1/ModelDuck.java
+++ dp/src/a_pattern1/ModelDuck.java
@@ -7,5 +7,17 @@ public class ModelDuck extends Duck{
 		// TODO Auto-generated method stub
 		System.out.println("Model");
 	}
+	
+	@Override
+	public void fly() {
+		System.out.println("날지 못해요..");
+	}
+
+	@Override
+	public void quack() {
+		// TODO Auto-generated method stub
+		System.out.println("모델모델");
+	}
+	
 
 }
