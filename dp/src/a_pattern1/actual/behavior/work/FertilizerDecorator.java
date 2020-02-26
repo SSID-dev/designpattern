3baf6d3c9d2e1a24b284bb5d523ffb01b00403ea
Keunhwa Lee <drg9830@gmail.com>
2020-02-26 16:58:54 +0900

    [Design Pattern 3_Decorator] Fertilizer, Fertilizing, FertilizerDecorator 생성

---
+++ dp/src/a_pattern1/actual/behavior/work/FertilizerDecorator.java
@@ -0,0 +1,6 @@
+package a_pattern1.actual.behavior.work;
+
+public abstract class FertilizerDecorator extends Fertilizer{
+	public abstract String getDescription();
+	public abstract int fertilize();
+}
---
+++ dp/src/a_pattern1/actual/behavior/work/Fertilizing.java
@@ -0,0 +1,14 @@
+package a_pattern1.actual.behavior.work;
+
+import a_pattern1.actual.behavior.WorkBehavior;
+
+public class Fertilizing implements WorkBehavior{
+	
+	Fertilizer f;
+
+	@Override
+	public void work() {
+		System.out.println(f.getDescription());
+		System.out.println(f.fertilize() + "회복되었습니다.");
+	}
+}
